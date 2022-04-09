9ca8eea6949289feed0a86e98bf5a832332b3578
javi-paul <javierpaul2000@hotmail.com>
2022-04-09 13:34:45 +0200

    small improvements

---
+++ src/main/java/com/protocols/pollution/gui/PollutionSimProperties.java
@@ -0,0 +1,13 @@
+package com.protocols.pollution.gui;
+
+import java.util.Properties;
+import java.util.ResourceBundle;
+
+public class PollutionSimProperties {
+
+	public void storeParameters(Properties p, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
